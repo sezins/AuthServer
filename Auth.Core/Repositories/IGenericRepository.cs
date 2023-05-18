66cbbd3464f97e1103d9ba5966b053a4cf986420
sezin salli <sezininyayla@gmail.com>
2023-05-18 12:06:20 +0300

    updated

---
+++ Auth.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Core.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+    }
+}

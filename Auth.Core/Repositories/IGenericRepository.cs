bb5fe59c45ce93f31b7e9af4601ec62d29630355
sezin salli <sezininyayla@gmail.com>
2023-05-18 15:08:09 +0300

    Updated

--- Auth.Core/Repositories/IGenericRepository.cs
+++ Auth.Core/Repositories/IGenericRepository.cs
@@ -2,13 +2,21 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Auth.Core.Repository
 {
-    public interface IGenericRepository<T> where T : class
+    public interface IGenericRepository<TEntity> where TEntity : class
     {
-        Task<T> GetByIdAsync(int id);
+        Task<TEntity> GetByIdAsync(int id);
+        Task<IQueryable<TEntity>> GetAllAsync();
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+        Task AddAsync(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Update(TEntity entity);
+
     }
 }
